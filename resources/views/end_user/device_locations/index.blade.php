1be883871e730103b3865b953dd271e9261cba1e
muhrizkiakbar <muhrizkiakbar@live.com>
2025-03-02 22:31:20 +0800

    a

--- resources/views/end_user/device_locations/index.blade.php
+++ resources/views/end_user/device_locations/index.blade.php
@@ -84,7 +84,7 @@
             label.style.fontSize = '12px';
             label.style.borderRadius = '4px';
             label.style.textAlign = 'center';
-            label.style.transform = 'translate(-50%, -30px)';
+            label.style.transform = 'translate(-50%, -50px)';
 
             // Tambahkan elemen label sebagai custom marker
             new window.maplibregl.Marker({ element: label })
