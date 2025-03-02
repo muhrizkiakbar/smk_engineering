a5c86c87bf46410bed0558fd1aed8967f3a14158
muhrizkiakbar <muhrizkiakbar@live.com>
2025-03-02 22:39:19 +0800

    a

--- resources/views/end_user/device_locations/index.blade.php
+++ resources/views/end_user/device_locations/index.blade.php
@@ -84,9 +84,10 @@
             label.style.fontSize = '12px';
             label.style.borderRadius = '4px';
             label.style.textAlign = 'center';
+            label.style.transform = 'translate(-50%, -200px)';
 
             // Tambahkan elemen label sebagai custom marker
-            new window.maplibregl.Marker({ element: label, offset: [0, -200] })
+            new window.maplibregl.Marker({ element: label })
                 .setLngLat([device.longitude, device.latitude])
                 .addTo(map);
         });
