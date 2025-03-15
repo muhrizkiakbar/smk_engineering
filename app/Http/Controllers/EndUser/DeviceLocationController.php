11d737a5cafd62183ebde6fce1be1b90c63e71aa
muhrizkiakbar <muhrizkiakbar@live.com>
2025-03-15 16:35:07 +0800

    a

--- app/Http/Controllers/EndUser/DeviceLocationController.php
+++ app/Http/Controllers/EndUser/DeviceLocationController.php
@@ -160,7 +160,7 @@ class DeviceLocationController extends Controller
         );
 
         $device_location = DeviceLocation::find($device_location_id);
-        $device_photos = DevicePhoto::where('device_location_id', $device_location_id)->cursorPaginate(10);
+        $device_photos = DevicePhoto::where('device_location_id', $device_location_id)->orderBy('created_at', 'asc')->cursorPaginate(10);
         $device_photo = DevicePhoto::where('state', 'active')->where('device_location_id', $device_location_id)->orderby('created_at', 'desc')->first();
         return view(
             'end_user.device_locations.device_photo',
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,8 +9,6 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
-        <!-- Font Awesome Icons -->
-        <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
         <!-- Popper -->
         <script src="https://unpkg.com/@popperjs/core@2"></script>
         <style>
