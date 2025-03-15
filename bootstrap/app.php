7edb7982004d6ff05be05dd9d9f05bec73069749
muhrizkiakbar <muhrizkiakbar@live.com>
2025-03-15 13:38:12 +0800

    a

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\JwtMiddleware;
+use App\Http\Middleware\TrustProxies;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->prepend(TrustProxies::class);
         $middleware->appendToGroup('auth:api', JwtMiddleware::class);
         $middleware->alias([
            'admin' => \App\Http\Middleware\AdminMiddleware::class,
