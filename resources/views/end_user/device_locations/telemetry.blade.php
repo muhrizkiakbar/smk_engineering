4adfdc82117673edc6d19fc778f500e1adb4a037
muhrizkiakbar <muhrizkiakbar@live.com>
2025-03-21 09:38:12 +0800

    a

--- resources/views/end_user/device_locations/telemetry.blade.php
+++ resources/views/end_user/device_locations/telemetry.blade.php
@@ -257,7 +257,7 @@
                 ">
                     <i class="fa-solid text-3xl fa-water"></i>
                 </div>
-                <div class="stat-title">Water Height</div>
+                <div class="stat-title">Water Level</div>
                 <div id="water_height_card" class="stat-value
                     @if ($current_device_location->device->has_water_height == true)
                         text-primary
